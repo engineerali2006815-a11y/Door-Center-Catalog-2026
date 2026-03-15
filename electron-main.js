771d3cd6873886f25401c971afc9513131009599
Engineer Ali <engineer.ali2006815@gmail.com>
2026-03-15 22:23:12 +0000

    كيف انقل التطبيق الى ملف exe

---
+++ electron-main.js
@@ -0,0 +1,30 @@
+const { app, BrowserWindow } = require('electron');
+const path = require('path');
+
+function createWindow() {
+  const win = new BrowserWindow({
+    width: 1200,
+    height: 800,
+    webPreferences: {
+      nodeIntegration: false,
+      contextIsolation: true,
+    },
+    title: "Door Center | مركز الأبواب",
+    autoHideMenuBar: true
+  });
+
+  // في مرحلة التطوير نشغل رابط السيرفر المحلي
+  win.loadURL('http://localhost:9002');
+}
+
+app.whenReady().then(() => {
+  createWindow();
+
+  app.on('activate', () => {
+    if (BrowserWindow.getAllWindows().length === 0) createWindow();
+  });
+});
+
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') app.quit();
+});
